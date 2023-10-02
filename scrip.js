f2fc39a5c0d6258e3a393125d7193b3a4be30518
ekariyanto <145081056+ekariyanto@users.noreply.github.com>
2023-10-02 17:04:10 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,11 @@
+// var nilai = 2;
+// if(nilai < 5){
+//    conse(nilai + " Lebih kecil dari 5")
+// }
+var nilai = prompt("Masukan nilai yang anda inginkan ")
+
+if(nilai  % 2 == 0){
+    alert(nilai + " adalah bilangan genap")
+ } else if(nilai % 2 == 1){
+    alert(nilai + " adalah bilang ganjil")
+ }
